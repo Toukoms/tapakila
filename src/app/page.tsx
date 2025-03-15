b0fe922071a57e219ccab0fb7686eeb4ad769240
Toukoms <tokiniaina090605@gmail.com>
2025-03-15 10:08:44 +0300

    Feat: ticket view in event detail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Events from "@/components/Events";
 import Hero from "@/components/Hero";
-import Tickets from "@/components/Tickets";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Events />
-      <Tickets />
     </div>
   );
 }

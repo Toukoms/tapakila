ff95ed94f6e30b6cd4f08f8605e18e983200d0ef
Toukoms <tokiniaina090605@gmail.com>
2025-03-21 23:20:18 +0300

    Fix: Remove unused component

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,5 +1,5 @@
-import { events } from "@/constants/events";
-import Collection from "./Collection";
+// import { events } from "@/constants/events";
+// import Collection from "./Collection";
 import Filter from "./Filter";
 
 function Events() {
@@ -7,11 +7,11 @@ function Events() {
     <div id="events" className="p-4 py-10 sm:p-6 md:p-10 md:py-14">
       <h2 className="text-3xl font-bold font-mono mb-6">Upcoming Events</h2>
       <Filter />
-      <Collection
+      {/* <Collection
         type={"All_events"}
         totalPages={events.length}
         data={events}
-      />
+      /> */}
     </div>
   );
 }

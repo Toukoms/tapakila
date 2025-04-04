cf0544fd54e8e6d43621ca27a0ac9634c2cdb683
Toukoms <tokiniaina090605@gmail.com>
2025-04-04 14:19:38 +0300

    refactor

--- src/features/events/containers/FeaturedEventsContainer.tsx
+++ src/features/events/containers/FeaturedEventsContainer.tsx
@@ -1,7 +1,6 @@
 "use client";
 import useSWR from "swr";
 import EventGrid from "../components/EventGrid";
-import { Suspense } from "react";
 
 export default function FeaturedEventsContainer() {
   const { error, data, isLoading } = useSWR("/events/allEventsId");
@@ -13,9 +12,7 @@ export default function FeaturedEventsContainer() {
       ) : error ? (
         <p>Error getting Events</p>
       ) : data && data.length > 0 ? (
-        <Suspense fallback={<div>Loading events...</div>}>
-          <EventGrid type="feat" />
-        </Suspense>
+        <EventGrid type="feat" />
       ) : (
         <div className="text-center">
           <p className="text-xl font-medium mb-4">No events for the moment</p>
